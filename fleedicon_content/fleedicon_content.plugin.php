5b346272b9a30be3cd8e5a970141040e0d036765
Simounet <contact@simounet.net>
2014-03-12 18:09:53 +0100

    Remove useless disabled file term + warning for waiting time on install

--- fleedicon_content/fleedicon_content.plugin.php
+++ fleedicon_content/fleedicon_content.plugin.php
@@ -2,30 +2,22 @@
 
 /*
   @name Fleedicon_content
-  @author gavrochelegnou <gavrochelegnou@trashmail.net>
   @author Simounet <contact@simounet.net>
+  @initby gavrochelegnou <gavrochelegnou@trashmail.net>
   @licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
-  @version 1.5.0
-  @description Le plugin Fleedicon_content ajoute un favicon à gauche de chaque item lors de la lecture
+  @version 2.0.0
+  @description Add a the site's favicon on the left of each feed (setup can take a while if you have many feeds)
  */
 
 
 require( 'classes/Fleedicon.php' );
 
-/*
- * Affiche un favicon en fonction d'un objet "event"
- * @param event $event
- */
 function fleedicon_content_plugin_addFavicon(&$event) {
     $path = Plugin::path();
     $fleedicon = new Fleedicon($event->getFeed(), $path);
     $event->favicon = $fleedicon->action();
 }
 
-/**
- * Affiche un favicon en fonction d'un tableau "feed"
- * @param type $feed
- */
 function fleedicon_aside_plugin_addFavicon(&$feed) {
     $path = Plugin::path();
     $fleedicon = new Fleedicon($feed['id'], $path);
@@ -33,13 +25,7 @@ function fleedicon_aside_plugin_addFavicon(&$feed) {
 }
 
 
-/**
- * Ajout de l'icone à coté de chaque item
- */
 Plugin::addHook("event_pre_title", "fleedicon_content_plugin_addFavicon");
 
-/**
- * Ajout de l'icone à coté du flux
- */
 Plugin::addHook("menu_pre_feed_link", "fleedicon_aside_plugin_addFavicon");
 
