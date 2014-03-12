287e62652a9c2d172d0f74d56ff3d1bd607d8fbf
Simounet <contact@simounet.net>
2014-03-12 18:01:37 +0100

    Install and uninstall updated Add/remove check file with last checking date Install now start get favicons

--- fleedicon_content/install.php
+++ fleedicon_content/install.php
@@ -1,11 +1,17 @@
 <?php
 
-/**
- * Création du dossier de favicons
- */
-if (!file_exists(Plugin::path() . 'favicons/')) {
-    $res = mkdir(Plugin::path() . 'favicons/');
-    if(!$res) {
-        echo 'Impossible de créer le dossier pour stocker les favicons, vérifiez les droits sur le serveur';
-    }
+require_once('classes/Fleedicon.php');
+$plugins_path = Plugin::path();
+
+touch($plugins_path.'check');
+
+$feed = new Feed();
+$conditions = 'SELECT id, website FROM `' . MYSQL_PREFIX .  'feed` ;';
+$query = $feed->customQuery($conditions);
+
+while( $feed = mysql_fetch_assoc($query) ) {
+    $fleeicon = new Fleedicon($feed['id'], $plugins_path);
+    $fleeicon->setFavicon($feed['website']);
 }
+
+?>
--- fleedicon_content/uninstall.php
+++ fleedicon_content/uninstall.php
@@ -1,14 +1,11 @@
 <?php
 
-/**
- * Création du dossier de favicons
- */
-
-$pluginsPath = Plugin::path() . 'favicons/';
-if (file_exists($pluginsPath)) {
-    $favicons = glob($pluginsPath.'*');
+$plugins_path = Plugin::path();
+$favicons_path = $plugins_path.'favicons/';
+if (file_exists($favicons_path)) {
+    $favicons = preg_grep('/default\.png$/', glob($favicons_path.'*'), PREG_GREP_INVERT);
     foreach ($favicons as $favicon) {
         unlink($favicon);        
     }
-    unlink($pluginsPath);
+    unlink($plugins_path.'check');
 }
