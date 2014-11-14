b95e9f2fe9b141df5982cfd313d92da626de4438
Simounet <contact@simounet.net>
2014-11-14 18:06:00 +0100

    Api : constantAPI.php supprimé et gestion simplifiée

--- api/uninstall.php
+++ api/uninstall.php
@@ -1,9 +1,5 @@
 <?php
 
-$constant = "<?php
-define('PLUGIN_ENABLED','0');
-?>";
-
-file_put_contents(Plugin::path().'constantAPI.php', $constant);
+unlink( Plugin::path().'constantAPI.php' );
 
 ?>
